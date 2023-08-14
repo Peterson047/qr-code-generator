869ebc748d5023eab33165c4dbed9ea30b884215
Peterson Alves <peterrson047@gmail.com>
2023-08-14 08:57:14 -0300

    adiciona o R$ e casa decimal
    falta corrigir a responsividade do H2

--- CPM-CALCULATOR/script.js
+++ CPM-CALCULATOR/script.js
@@ -1,37 +1,18 @@
-
-function calculateInvestment(rowNumber, multiplier = 60) {
-    const impressionsInput = document.getElementById(`impressions${rowNumber}`);
-    const investmentInput = document.getElementById(`investment${rowNumber}`);
-
-    const impressionsValue = parseFloat(impressionsInput.value);
-    if (isNaN(impressionsValue)) {
-        investmentInput.value = "0.00";
-    } else {
-        const investmentValue = impressionsValue * multiplier;
-        investmentInput.value = investmentValue.toFixed(2);
-    }
+function formatNumberWithThousandsSeparator(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
 
-// Calculate all investments when the page loads
-window.onload = function () {
-    calculateInvestment(1);
-    calculateInvestment(2);
-    calculateInvestment(3, 200);
-    calculateInvestment(4, 150);
-};
-
-
-
 function calculateInvestment(rowNumber, multiplier = 60) {
     const impressionsInput = document.getElementById(`impressions${rowNumber}`);
     const investmentInput = document.getElementById(`investment${rowNumber}`);
 
-    const impressionsValue = parseFloat(impressionsInput.value);
+    const impressionsValue = parseFloat(impressionsInput.value.replace(/\./g, '').replace(',', '.'));
     if (isNaN(impressionsValue)) {
         investmentInput.value = "0.00";
     } else {
         const investmentValue = impressionsValue * multiplier;
-        investmentInput.value = investmentValue.toFixed(2);
+        impressionsInput.value = formatNumberWithThousandsSeparator(impressionsValue.toFixed(0));
+        investmentInput.value = `R$: ${formatNumberWithThousandsSeparator(investmentValue.toFixed(2))}`;
     }
 }
 
